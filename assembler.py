415748310301af9ab424306366f4c2dc0733a825
chiud3UCI <chiud3@uci.edu>
2025-11-25 13:34:58 -0800

    Implemented Assembler
    Labels not supported yet

---
+++ assembler.py
@@ -0,0 +1,167 @@
+import sys
+from collections import deque
+from instructions import Instructions
+
+DEBUG_PRINT = True
+
+# register names are in order based on RISC-V spec
+ABI_NAMES = [
+    "zero", "ra", "sp", "gp", "tp",
+    "t0", "t1", "t2", "s0", "s1",
+    "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7",
+    "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11",
+    "t3", "t4", "t5", "t6"
+]
+
+# construct a lookup table that maps register name to register number
+REGISTER_LOOKUP = {name : i for i, name in enumerate(ABI_NAMES)}
+
+# splits line into tokens.
+# a token is a group of contiguous characters that are alphanumeric, "_", or "-" 
+# anything else is a delimiter
+# supports hashtag comments at the end of the line.
+def assembler_tokenize(line: str) -> deque[str]:
+    tokens = [[]]
+    for char in line:
+        if char == "#":
+            break
+        elif char.isalnum() or char == "_" or char == "-":
+            tokens[-1].append(char)
+        elif len(tokens[-1]) > 0:
+            tokens.append([])
+    tokens = deque("".join(char_list) for char_list in tokens)
+    if len(tokens[-1]) == 0:
+        tokens.pop()
+    return tokens
+
+# converts assembly instruciton to 32 bit machine code
+def assembler_parse_line(line: str) -> str:
+    if DEBUG_PRINT:
+        print(f"Parsing: {line}")
+
+    tokens = assembler_tokenize(line)
+
+    if DEBUG_PRINT:
+        print(f"Tokens: {list(tokens)}")
+
+    binary_strings = deque()
+
+    # first token from left is always the instruction name
+    instr_name = tokens.popleft().upper()
+    opcode = Instructions[instr_name].value
+    opcode_str = f"{opcode:b}".zfill(7)
+    binary_strings.appendleft(opcode_str)
+
+    def process_reg():
+        reg_name = tokens.popleft().lower()
+        reg_num = REGISTER_LOOKUP[reg_name]
+        binary_strings.appendleft(f"{reg_num:b}".zfill(6))
+
+    def process_imm(jal=False):
+        imm = int(tokens.popleft())
+        # python can't do 2's complement so I have to do it myself
+        if (imm < 0):
+            li = list(f"{-imm:b}")
+            flip = False
+            for i in reversed(range(len(li))):
+                if flip:
+                    li[i] = "1" if li[i] == "0" else "0"
+                elif li[i] == "1":
+                    flip = True
+            bin = "".join(li)
+        else:
+            bin = f"{imm:b}"
+        # need to pad with 1s or 0s based on sign
+        pad = jal and 25 or 11
+        binary_strings.appendleft(bin.rjust(pad, "1" if imm < 0 else "0"))
+
+    # NO_OP
+    if instr_name == "NO_OP":
+        pass
+    # LW
+    elif instr_name == "LW":
+        process_reg()  # rd
+        process_imm()  # imm
+        process_reg()  # rs1
+        # need to swap imm and rs1 to comply with format
+        d = binary_strings
+        d[0], d[1] = d[1], d[0]
+    # SW
+    elif instr_name == "SW":
+        process_reg()  # rs1
+        process_imm()  # imm
+        process_reg()  # rs2
+        # need to swap imm and rs2 to comply with format
+        d = binary_strings
+        d[0], d[1] = d[1], d[0]
+    # JAL
+    # TODO: add support for labels
+    elif instr_name == "JAL":
+        process_imm(True)  # imm
+    # R-Type
+    elif opcode_str[1:4] == "001":
+        process_reg()  # rd
+        process_reg()  # rs1
+        process_reg()  # rs2
+    # I-Type
+    elif opcode_str[1:4] == "011":
+        process_reg()  # rd
+        process_reg()  # rs1
+        process_imm()  # imm
+    # Branch
+    elif opcode_str[1:4] == "011":
+        process_reg()  # rs1
+        process_reg()  # rs2
+        process_imm()  # imm
+
+    if DEBUG_PRINT:
+        print(f"Binary Strings: {list(binary_strings)}")
+
+    result = "".join(binary_strings).zfill(32)
+
+    if DEBUG_PRINT:
+        print(f"Machine Code: {result}\n")
+
+    return result
+
+# =====================================================================================
+# USAGE: python assembler.py <source assembly filename> <destination binary filename>
+# (destination binary file can be omitted if you only want to verify console output)
+# =====================================================================================
+if __name__ == "__main__":
+    src_filename = sys.argv[1]
+    dest_filename = None
+    if (len(sys.argv) >= 3):
+        dest_filename = sys.argv[2]
+
+    machine_codes = []
+
+    with open(src_filename, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            # get rid of newlines and hashtag comments
+            line = line.strip()
+            index = line.find("#")
+            if index != -1:
+                line = line[0:index]
+
+            # only parse non-empty lines
+            if len(line) > 0:
+                machine_codes.append(assembler_parse_line(line))
+
+    byte_array = []
+    for word in machine_codes:
+        byte_array.append(int(word[0:8], 2))
+        byte_array.append(int(word[8:16], 2))
+        byte_array.append(int(word[16:24], 2))
+        byte_array.append(int(word[24:32], 2))
+
+    if (DEBUG_PRINT):
+        print(f"bytes to be written into binary file:\n{byte_array}")
+
+    if dest_filename:
+        with open(dest_filename, "wb") as f:
+            f.write(bytes(byte_array))
+
+
+
---
+++ test.asm
@@ -0,0 +1,7 @@
+# please ignore this comment
+
+lw s0, 8(s1) # test comment
+no_op cat dog
+            # please ignore this comment
+addi s0, zero, 1000#comment
+addi s0, zero, -1000
