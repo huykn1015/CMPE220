6605659cf3e1b92778d55c4eff5daddb9eaea5eb
DanielChiuSJSU <daniel.chiu01@sjsu.edu>
2025-11-25 18:32:58 -0800

    Added Label Support

--- assembler.py
+++ assembler.py
@@ -16,6 +16,63 @@ ABI_NAMES = [
 # construct a lookup table that maps register name to register number
 REGISTER_LOOKUP = {name : i for i, name in enumerate(ABI_NAMES)}
 
+# 1. remove newlines and comments from each line
+# 2. identify labels and link them with the instruction
+#   - labels are anything before a colon
+def assembler_preprocess(lines: list[str]) -> list[str, list[str]]:
+    line_entries: list[str, list[str]] = []
+    for line in lines:
+        # remove hashtag comments
+        index = line.find("#")
+        if (index != -1):
+            line = line[:index]
+        # remove extra spaces and newlines
+        line = line.strip()
+        # identify label
+        index = line.find(":")
+        label = ""
+        if (index != -1):
+            label = line[:index].strip().upper()
+            if index == len(line)-1:
+                line = ""
+            else:
+                line = line[index+1:]
+        line_entries.append([
+            line,
+            [label] if label else []
+        ])
+
+    if DEBUG_PRINT:
+        print("raw line entries:")
+        for entry in line_entries:
+            print(f"{entry}")
+        print()
+
+    # remove entries with empty line 
+    # if removed entry has labels, transfer those labels to
+    #   the next entry with non-empty lines
+    revised_entries = []
+    orphaned_labels = []
+    for entry in line_entries:
+        line, labels = entry
+        # non-empty line
+        if line:
+            labels.extend(orphaned_labels)
+            orphaned_labels.clear()
+            revised_entries.append(entry)
+        # empty line
+        else:
+            orphaned_labels.extend(labels)
+
+    if DEBUG_PRINT:
+        print("revised line entries:")
+        for entry in revised_entries:
+            print(f"{entry}")
+        print()
+
+    return revised_entries
+
+
 # splits line into tokens.
 # a token is a group of contiguous characters that are alphanumeric, "_", or "-" 
 # anything else is a delimiter
@@ -23,9 +80,7 @@ REGISTER_LOOKUP = {name : i for i, name in enumerate(ABI_NAMES)}
 def assembler_tokenize(line: str) -> deque[str]:
     tokens = [[]]
     for char in line:
-        if char == "#":
-            break
-        elif char.isalnum() or char == "_" or char == "-":
+        if char.isalnum() or char == "_" or char == "-":
             tokens[-1].append(char)
         elif len(tokens[-1]) > 0:
             tokens.append([])
@@ -35,9 +90,9 @@ def assembler_tokenize(line: str) -> deque[str]:
     return tokens
 
 # converts assembly instruciton to 32 bit machine code
-def assembler_parse_line(line: str) -> str:
+def assembler_parse_line(index: int, line: str, label_lookup: dict[str, int]) -> str:
     if DEBUG_PRINT:
-        print(f"Parsing: {line}")
+        print(f"Parsing Instruction {index}: {line}")
 
     tokens = assembler_tokenize(line)
 
@@ -49,16 +104,35 @@ def assembler_parse_line(line: str) -> str:
     # first token from left is always the instruction name
     instr_name = tokens.popleft().upper()
     opcode = Instructions[instr_name].value
-    opcode_str = f"{opcode:b}".zfill(7)
+    opcode_str = f"{opcode:07b}"
     binary_strings.appendleft(opcode_str)
 
     def process_reg():
         reg_name = tokens.popleft().lower()
         reg_num = REGISTER_LOOKUP[reg_name]
-        binary_strings.appendleft(f"{reg_num:b}".zfill(6))
+        binary_strings.appendleft(f"{reg_num:06b}")
 
-    def process_imm(jal=False):
-        imm = int(tokens.popleft())
+    # branch = 0: I-type, imm is literal integer
+    # branch = 1: Branch, imm is a label and will translate to offset
+    # branch = 2: Jump, imm is a label and will translate to direct address
+    def process_imm(branch=0):
+        token = tokens.popleft()
+        imm = 0
+        if branch == 0:  # I-Type
+            imm = int(token)
+        elif branch == 1:  # branch
+            label = token.upper()
+            target_index = label_lookup[label]
+            imm = target_index - index
+            if DEBUG_PRINT:
+                print(f"branch offset: {imm}")
+        else:  # jump
+            label = token.upper()
+            target_index = label_lookup[label]
+            imm = target_index
+            if DEBUG_PRINT:
+                print(f"jump target: {imm}")
+
         # python can't do 2's complement so I have to do it myself
         if (imm < 0):
             li = list(f"{-imm:b}")
@@ -72,7 +146,7 @@ def assembler_parse_line(line: str) -> str:
         else:
             bin = f"{imm:b}"
         # need to pad with 1s or 0s based on sign
-        pad = jal and 25 or 11
+        pad = (branch == 2) and 25 or 11
         binary_strings.appendleft(bin.rjust(pad, "1" if imm < 0 else "0"))
 
     # NO_OP
@@ -95,9 +169,8 @@ def assembler_parse_line(line: str) -> str:
         d = binary_strings
         d[0], d[1] = d[1], d[0]
     # JAL
-    # TODO: add support for labels
     elif instr_name == "JAL":
-        process_imm(True)  # imm
+        process_imm(2)  # imm
     # R-Type
     elif opcode_str[1:4] == "001":
         process_reg()  # rd
@@ -109,10 +182,10 @@ def assembler_parse_line(line: str) -> str:
         process_reg()  # rs1
         process_imm()  # imm
     # Branch
-    elif opcode_str[1:4] == "011":
+    elif opcode_str[1:4] == "110":
         process_reg()  # rs1
         process_reg()  # rs2
-        process_imm()  # imm
+        process_imm(1)  # imm
 
     if DEBUG_PRINT:
         print(f"Binary Strings: {list(binary_strings)}")
@@ -134,20 +207,24 @@ if __name__ == "__main__":
     if (len(sys.argv) >= 3):
         dest_filename = sys.argv[2]
 
-    machine_codes = []
-
+    lines = []
     with open(src_filename, "r") as f:
         lines = f.readlines()
-        for line in lines:
-            # get rid of newlines and hashtag comments
-            line = line.strip()
-            index = line.find("#")
-            if index != -1:
-                line = line[0:index]
+    
+    line_entries = assembler_preprocess(lines)
+    # extract lines from first column of line_entries
+    # and setup a lookup that maps label to current line lumber
+    lines = []
+    label_lookup: dict[str, int] = {}
+    for i, (line, labels) in enumerate(line_entries):
+        for label in labels:
+            label_lookup[label] = i
+        lines.append(line)
 
-            # only parse non-empty lines
-            if len(line) > 0:
-                machine_codes.append(assembler_parse_line(line))
+    print(label_lookup)
+    machine_codes = []
+    for index, line in enumerate(lines):
+        machine_codes.append(assembler_parse_line(index, line, label_lookup))
 
     byte_array = []
     for word in machine_codes:
--- test.asm
+++ test.asm
@@ -1,7 +1,10 @@
 # please ignore this comment
-
+LABEL1:
+LABEL2:
 lw s0, 8(s1) # test comment
 no_op cat dog
             # please ignore this comment
-addi s0, zero, 1000#comment
+LABEL3:addi s0, zero, 1000#comment
 addi s0, zero, -1000
+beq zero, zero, LABEL3
+jal LABEL1
